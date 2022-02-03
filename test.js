91750d11fe32f1aed669c3403b014f7fefceb59f
Maria Tomeyan <maria.tomeyan@gmail.com>
2022-02-03 23:55:16 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-// ******************** TASK 1 ********************
+// // ******************** TASK 1 ********************
 const task1 = [
   "apple",
   "pear",
@@ -14,23 +14,20 @@ const task1 = [
   "pear",
   "pear",
 ];
-
-// Count how many Apples, Pears, Lemons, Oranges, Pineapples, Tomatoes, Mangos and Bananas are in the list.
+//
+// // Count how many Apples, Pears, Lemons, Oranges, Pineapples, Tomatoes, Mangos and Bananas are in the list.
 
 function countItems(item, items) {
-    var key = item.charAt(0).toUpperCase() + item.slice(1)
-    return {
-    [key]:  items.filter((x) => x.toLowerCase() === item.toLowerCase()).length,
-    };
+  var key = item.charAt(0).toUpperCase() + item.slice(1);
+  return {
+    [key]: items.filter((x) => x.toLowerCase() === item.toLowerCase()).length,
+  };
 }
 
-(function(){
- var res = countItems('apple', task1)
- console.log(res)
+(function () {
+  // var res = countItems('apple', task1)
+  // console.log(res)
 })();
-
-
-
 
 // ******************** TASK 2 ********************
 
@@ -38,64 +35,61 @@ function countItems(item, items) {
 // b) Write a solution that has better performance
 // c) What is the performance of your new solution?
 
-const domains = { // Example to show data shape only.
-    "one.com": { policy: "block" },
-    "two.com": { policy: "none" },
-    "three.com": { policy: "none" },
-    "four.com": { policy: "block" },
-}
-
-const getBlockPolicyState = (domains) => {
-    numDomains = Object.keys(domains).length;
-    anyBlockedDomain = false;
-    areAllDomainsBlocked = false;
-    blockedDomains = []
-
-    for (let i = 0; i < numDomains; i++) {
-         if(Object.entries(domains)[i][1].policy === "block") {
-             anyBlockedDomain = true
-             blockedDomains.push(Object.entries(domains)[i])
-         }
-    }
-    areAllDomainsBlocked = blockedDomains.length === numDomains ? true : false
-    return { anyBlockedDomain, areAllDomainsBlocked };
+const domains = {
+  // Example to show data shape only.
+  "one.com": { policy: "block" },
+  "two.com": { policy: "block" },
+  "three.com": { policy: "none" },
+  "four.com": { policy: "block" },
 };
 
+const getBlockPolicyState = (domains) => {
+  numDomains = Object.keys(domains).length;
+  anyBlockedDomain = false;
+  areAllDomainsBlocked = false;
 
-(function(){
-   console.log(getBlockPolicyState(domains))
+  blockedDomains = Object.entries(domains).filter((domain) => {
+    return domain[1].policy === "block";
+  });
+
+  numBlockedDomains = blockedDomains.length;
+  areAllDomainsBlocked = numBlockedDomains === numDomains ? true : false;
+  anyBlockedDomain = numBlockedDomains >= 1 ? true : false;
+
+  return { areAllDomainsBlocked };
+};
+
+(function () {
+  console.time();
+  console.log(getBlockPolicyState(domains));
+  console.timeEnd();
 })();
-
-
 
 // ******************** TASK 3 ********************
 
 // Find the first recurring character of the below lists
 let task8 = [
-    [2,5,1,2,3,5,1,2,4],
-    [2,1,1,2,3,5,1,2,4],
-    [2,3,4,5],
-    [2,5,5,2,3,5,1,2,4]
-]
+  [2, 5, 1, 2, 3, 5, 1, 2, 4],
+  [2, 1, 1, 2, 3, 5, 1, 2, 4],
+  [2, 3, 4, 5],
+  [2, 5, 5, 2, 3, 5, 1, 2, 4],
+];
 
 function getFirstRecurringCharacter(list) {
-    let hash = new Set();
+  let hash = new Set();
 
-    for (let i = 0; i <= list.length - 1; i++) {
-        let character = list[i];
-        if (hash.has(character))
-            return character;
-        else
-            hash.add(character);
-    }
+  for (let i = 0; i <= list.length - 1; i++) {
+    let character = list[i];
+    if (hash.has(character)) return character;
+    else hash.add(character);
+  }
 
-    return undefined;
+  return undefined;
 }
 
-(function(){
-    task8.map(item => {
-     let res = getFirstRecurringCharacter(item)
-        console.log('Recurring character is: ', res)
-    })
+(function () {
+  task8.map((item) => {
+    let res = getFirstRecurringCharacter(item);
+    console.log("Recurring character is: ", res);
+  });
 })();
-
